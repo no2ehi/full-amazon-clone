50eaadc8a6a1a6f80df2f015372d69b999a4dbf9
no2ehi <graphist77@gmail.com>
2023-04-19 18:18:47 +0430

    responsive cart page

--- components/CartPage/CartPage.tsx
+++ components/CartPage/CartPage.tsx
@@ -1,10 +1,11 @@
 import { useAppSelector } from "@/redux/hooks";
 import Product from "./Product";
 
-const CartPage = ({cart}: any) => {
-    return ( 
+const CartPage = ({ cart }: any) => {
+    let totalPrice = Number(cart.cartItems.reduce((total: any, product: any) =>  total + (product.price * product.qty) ,0)).toFixed(2);
+    return (
         <div className="grid grid-row-7 md:grid-cols-7 gap-4 px-2 md:px-4 py-4">
-            <div className="col-span-5 bg-white rounded py-2 px-4 border">
+            <div className="md:col-span-5 bg-white rounded py-2 px-4 border">
                 <h2 className="font-bold text-3xl my-2">Shopping Cart</h2>
                 <div className="w-full bg-slate-200 h-[1px]" />
                 {cart.cartItems.map((product: any, i: any) => (
@@ -12,16 +13,18 @@ const CartPage = ({cart}: any) => {
                 ))}
             </div>
 
-            <div className="col-span-2 bg-white rounded py-2 px-4 border">
-                <h3 className="text-xl my-2 font-semibold">Subtotal (1 item): $ 2</h3>
-
+            <div className="md:col-span-2 bg-white rounded py-2 px-4 border">
+                <h3 className="text-xl my-2 font-semibold">
+                    Subtotal ({cart.cartItems.length} item): {totalPrice}$
+                </h3>
                 <div>
-                    <button className="w-full my-4 py-2 px-4 bg-amazon-orange text-black rounded-full shadow">Add to Cart</button>
+                    <button className="w-full my-4 py-2 px-4 bg-amazon-orange text-black rounded-full shadow">
+                        Add to Cart
+                    </button>
                 </div>
-                
             </div>
         </div>
-     );
-}
- 
+    );
+};
+
 export default CartPage;
--- components/CartPage/Product.tsx
+++ components/CartPage/Product.tsx
@@ -7,7 +7,6 @@ import {
 } from "@heroicons/react/24/outline";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
 import { updateCart } from "../../redux/slices/CartSlice";
 
 const Product = ({ product }: any) => {
@@ -33,22 +32,22 @@ const Product = ({ product }: any) => {
     };
 
     return (
-        <div className="mt-2 grid grid-flow-col-dense grid-cols-6  border-b p-2 pb-4 last:border-none">
-            <div className="flex md:items-center">
+        <div className="mt-2 grid grid-cols-3  max-md:grid-rows-1 md:grid-cols-6  border-b p-2 pb-4 last:border-none ">
+            <div className="flex flex-col-reverse md:flex-row md:items-center">
                 <input
                     type="checkbox"
                     name="product"
-                    className="w-5 h-5 cursor-pointer"
+                    className="w-5 h-5 cursor-pointer hidden md:block"
                 />
                 <Image
                     src={product.images[0].url}
                     width={100}
                     height={100}
-                    className="ml-4 object-contained rounded-md outline outline-1 outline-offset-2 outline-slate-300"
+                    className="md:ml-4 object-contained rounded-md outline outline-1 outline-offset-2 outline-slate-300"
                     alt={product.name}
                 />
             </div>
-            <div className="col-span-4 md:ml-2">
+            <div className="col-span-2  md:col-span-4 md:ml-2">
                 <Link href="" target="_blank" className="text-sm font-semibold">
                     {product.name}
                 </Link>
@@ -62,15 +61,14 @@ const Product = ({ product }: any) => {
                         />
                     </div>
                     <span>{product.size}</span>
-                    <span>{(product.price).toFixed(2)}</span>
+                    <span>{product.price.toFixed(2)}</span>
                 </div>
                 <div className="flex items-center">
                     <span className="font-bold text-xl">
                         USD{(product.price * product.qty).toFixed(2)} $
                     </span>
                     <span className="ml-2 text-sm line-through text-slate-400">
-                        USD{(product.priceBefore * product.qty).toFixed(2)}{" "}
-                        $
+                        USD{(product.priceBefore * product.qty).toFixed(2)} $
                     </span>
                 </div>
                 <span className="text-blue-500 text-sm">
@@ -79,15 +77,20 @@ const Product = ({ product }: any) => {
                         : `+${product.shipping}$ shipping fee`}
                 </span>
             </div>
-            <div className="flex flex-col md:items-end">
-                <div className="flex items-center space-x-2">
+            <div className="max-md:col-span-3 max-md:items-center max-md:mt-2 max-md:justify-between flex md:mt-4  md:flex-col md:items-end md:justify-center">
+                <div className="flex items-center space-x-4">
+                    <input
+                        type="checkbox"
+                        name="product"
+                        className="w-5 h-5 cursor-pointer  md:hidden"
+                    />
                     <HeartIcon className="w-6 h-6 cursor-pointer" />
                     <TrashIcon
                         className="w-6 h-6 cursor-pointer"
                         onClick={() => removeItemCart(product._uid)}
                     />
                 </div>
-                <div className="mt-6  flex items-center space-x-2">
+                <div className="md:mt-4  flex items-center space-x-2">
                     <button
                         disabled={product.qty < 2}
                         className="bg-slate-200 p-1.5 rounded-full hover:bg-slate-300"
