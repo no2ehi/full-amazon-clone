786c948540d368991f67b3bfbba287cde64da45b
no2ehi <graphist77@gmail.com>
2023-05-01 11:39:13 +0430

    fix some bugs in deploy (change gitignore)

---
+++ request/upload.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export const uploadImages = async (formData) => {
+    const { data } = await axios.post('/api/cloudinary', formData, {
+        headers:{
+            "content-type": "multipart/form-data",
+        },
+    })
+
+    return data;
+}
---
+++ request/users.js
@@ -0,0 +1,57 @@
+import axios from "axios";
+
+export const saveCart = async (cart, user_id) => {
+    try {
+        const { data } = await axios.post("/api/user/savecart", {
+            cart,
+            user_id,
+        });
+        return data;
+    } catch (error) {
+        console.log("erorr >>>", error);
+    }
+};
+
+export const saveAddress = async (address) => {
+    try {
+        const { data } = await axios.post("/api/user/saveaddress", {
+            address,
+        });
+        return data;
+    } catch (error) {
+        console.log("erorr >>>", error.response.data.message);
+    }
+};
+
+export const changeActiveAddress = async (id) => {
+    try {
+        const { data } = await axios.put("/api/user/manageaddress", {
+            id,
+        });
+        return data;
+    } catch (error) {
+        console.log("erorr >>>", error.response.data.message);
+    }
+};
+
+export const deleteAddress = async (id) => {
+    try {
+        const { data } = await axios.delete("/api/user/manageaddress", {
+            data: { id },
+        });
+        return data;
+    } catch (error) {
+        console.log("erorr >>>", error.response.data.message);
+    }
+};
+
+export const applyCoupon = async (coupon) => {
+    try {
+        const { data } = await axios.post("/api/user/applycoupon", {
+            coupon,
+        });
+        return data;
+    } catch (error) {
+        return { message: error.response.data.message };
+    }
+};
